0a27195962659cb0ea535b1f10c517c1b9ee41fb
Janne Sauvala <janne.sauvala@gmail.com>
2017-01-15 15:10:26 +0200

    Socket.io implementation.

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,10 +2,26 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const buildFolder = '../../build';
 
 app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  console.log("Running in production mode");
+  app.use(express.static(path.join(__dirname, buildFolder)));
+}
+
 app.set('port', (process.env.PORT || 3001));
+
+server.listen(app.get('port'), () => {
+  console.log(`chatterbox backend server is running on port: ${app.get('port')}`);
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 app.post('/sendMessage', (request, response) => {
   console.log('/sendMessage from user: ' + request.body.user);
@@ -13,14 +29,7 @@ app.post('/sendMessage', (request, response) => {
   response.sendStatus(200)
 });
 
-app.listen(app.get('port'), () => {
-  console.log(`chatterbox backend server is running on port: ${app.get('port')}`);
-});
 
-if (process.env.NODE_ENV === 'production') {
-  console.log("Running in production mode");
-  app.use(express.static(path.join(__dirname, buildFolder)));
-}
 
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, buildFolder, 'index.html'));
